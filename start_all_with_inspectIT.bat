bb41a9fa0b5726045c3a8fe1faa2054a24fa806c
Tobias Angerstein <tobias.angerstein@novatec-gmbh.de>
2018-04-24 08:45:33 +0200

    Enable windows10 compatibility: Add C:\windows\system32 to PATH variable

--- start_all_with_inspectIT.bat
+++ start_all_with_inspectIT.bat
@@ -8,7 +8,7 @@ rem !!!!do not use spaces in the path of inspectIT installation folder!!!!
 set AGENTDIR=%1
 set WAITTIME=5
 set /a "x = 0"
-
+set PATH=%PATH%;C:\windows\system32;
 rem STARTTYPE, z.B. /B = start without open a new windows, /MIN start minized in new windows
 set STARTTYPE=/B
 
